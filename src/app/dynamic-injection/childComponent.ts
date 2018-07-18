c01932accbd2f879f6f43674850da5675703d98f
NarayanasamyP <34939696+NarayanasamyP@users.noreply.github.com>
2018-07-18 17:51:43 +0530

    Update childComponent.ts

--- src/app/dynamic-injection/childComponent.ts
+++ src/app/dynamic-injection/childComponent.ts
@@ -2,14 +2,18 @@ import {Component, ViewChild, TemplateRef} from '@angular/core';
 import { UploaderComponent } from '@syncfusion/ej2-ng-inputs';
 
 @Component({
-  selector: 'app-container',
+  selector: 'child-container',
   template: `
-      <ejs-uploader #fileUpload id="Uploader" autoUpload='false'></ejs-uploader>
+      <ejs-uploader #fileUpload id="Uploader" [asyncSettings]='path'  [autoUpload]= false ></ejs-uploader>
   `,
 })
 export class Newcomponent {
     @ViewChild('fileUpload')
     public itemTemplate: UploaderComponent;
+    public path: Object = {
+        saveUrl: 'https://aspnetmvc.syncfusion.com/services/api/uploadbox/Save',
+        removeUrl: 'https://aspnetmvc.syncfusion.com/services/api/uploadbox/Remove'
+    };
     constructor() {
     }
 }
